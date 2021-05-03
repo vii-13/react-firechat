95ce811caaba62d7fc2b9be463b75b62c45c9263
Esterlyn <diazesterlin62@gmail.com>
2021-05-02 23:11:30 -0400

    correccion de errores

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-
-import Button from './componets/Button';
-import {signInWithGoogle, signOut} from './login';
-import { useAuthState } from './hooks';
-import { firebase } from './config/firebase';
+import { Button, Channel } from "./componets";
+import { signInWithGoogle, signOut } from "./login";
+import { useAuthState } from "./hooks";
+import { firebase } from "./config/firebase";
 
 function App() {
-//Entegracion del hook useAuthState
-  const { user, initializing }= useAuthState(firebase.auth());
-// Renderizar el componente Button
+  //Entegracion del hook useAuthState
+  const { user, initializing } = useAuthState(firebase.auth());
+  // Renderizar el componente Button
   //Renderezar en funcion de la existencia de un usuario con operador ternario.
-const renderLoging=() => { 
-  if(initializing) {
-    return <div>
-      <h1>Loading...</h1>
-      </div>
-  }
-}
-return (
+  const renderLoging = () => {
+    if (initializing) {
+      return (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      );
+    }
+  };
+  return (
     <div>
-      {
-        user ? (
-          <>
+      {user ? (
+        <>
           <Button onClick={signOut}> Sing in with Google</Button>
           <p>Bienvenido al CHAT</p>
           <Channel user={user} />
-          </>
-        ) :<Button onClick={signInWithGoogle}> Sing in with Google</Button>
-      }
-
+        </>
+      ) : (
+        <Button onClick={signInWithGoogle}> Sing in with Google</Button>
+      )}
     </div>
   );
 }
--- src/componets/Channel.js
+++ src/componets/Channel.js
@@ -1,41 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { db } from "../config/firebase";
 
 const Channel = ({ user = null }) => {
-    const [messages, setMessages] = useState ([]);
+  const [messages, setMessages] = useState([]);
 
-    //Necesito cargar los mensajes desde la BD
-    useEffect(() => {
-        /*
+  //Necesito cargar los mensajes desde la BD
+  useEffect(() => {
+    /*
         crear el query de los mensajes.
         cargar los 100 mensajes ordenados por fecha.
         */
-       const query = db.collection('messages')
-       .orderBy('createdAt')
-       .limit(100);
-       const unsubscribe = query.onSnapshot(querysnapshot => {
-           //obtiene todos los mensajes desde la bd con su ID.
-        const data = querysnapshot.docs.map(doc => ({
-            ...doc.data(),
-            id: doc.id,
-        }));
+    const query = db.collection("messages").orderBy("createdAt").limit(100);
+    const unsubscribe = query.onSnapshot((querysnapshot) => {
+      //obtiene todos los mensajes desde la bd con su ID.
+      const data = querysnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
 
-        //Actualizo los mensajes ontenidos desde la bd.
-setMessages(data);
+      //Actualizo los mensajes ontenidos desde la bd.
+      setMessages(data);
+    });
+    //cleanup
+    return unsubscribe;
+  }, []);
 
-        });
-        //cleanup
-        return unsubscribe;
-
-           }, [])
-    
-
-    return <ul>
-        { messages.map(messages => (
-            //Todos los mensajes seran mostrados en una lista.
-            <li key={messages.id}>{messages.text}</li>
-        ))}
-    </ul>;
-
+  return (
+    <ul>
+      {messages.map((messages) => (
+        //Todos los mensajes seran mostrados en una lista.
+        <li key={messages.id}>{messages.text}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default Channel;
